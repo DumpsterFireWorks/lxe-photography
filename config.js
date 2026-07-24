e709f1e95eaa9ef0b3dc3acee267d5fbd8505ef3
Christopher Hilton <kool1160@gmail.com>
2026-07-24 14:04:09 -0400

    Add editable booking availability configuration

--- config.js
+++ config.js
@@ -10,5 +10,10 @@ window.LXE_CONFIG = {
     facebook: "",
     tiktok: "",
     pinterest: ""
-  }
+  },
+  availability: [
+    { date: "Saturday, August 8", times: ["10:00 AM", "1:00 PM", "4:30 PM"] },
+    { date: "Sunday, August 9", times: ["11:00 AM", "3:00 PM"] },
+    { date: "Saturday, August 15", times: ["9:30 AM", "12:30 PM", "5:00 PM"] }
+  ]
 };
