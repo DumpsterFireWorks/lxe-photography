3c9808843ebe2cdb666ff2763e419e04e4366b69
Christopher Hilton <kool1160@gmail.com>
2026-07-24 12:29:08 -0400

    feat: add editable business and social configuration

---
+++ config.js
@@ -0,0 +1,14 @@
+window.LXE_CONFIG = {
+  businessName: "LXE Photography",
+  photographer: "Lexus Erickson",
+  location: "Muskegon, Michigan",
+  email: "hello@lxephotography.com",
+  phone: "",
+  bookingUrl: "",
+  socials: {
+    instagram: "",
+    facebook: "",
+    tiktok: "",
+    pinterest: ""
+  }
+};
