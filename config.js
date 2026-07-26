43b5613359487d0b89ed55a3e88aa5cb5a843dff
Christopher Hilton <kool1160@gmail.com>
2026-07-26 07:26:14 -0400

    chore: remove fake availability and hide inactive social links

--- config.js
+++ config.js
@@ -3,17 +3,10 @@ window.LXE_CONFIG = {
   photographer: "Lexus Erickson",
   location: "Muskegon, Michigan",
   email: "hello@lxephotography.com",
-  phone: "",
-  bookingUrl: "",
+  inquiryEndpoint: "/api/inquiry",
   socials: {
     instagram: "",
     facebook: "",
-    tiktok: "",
-    pinterest: ""
-  },
-  availability: [
-    { date: "Saturday, August 8", times: ["10:00 AM", "1:00 PM", "4:30 PM"] },
-    { date: "Sunday, August 9", times: ["11:00 AM", "3:00 PM"] },
-    { date: "Saturday, August 15", times: ["9:30 AM", "12:30 PM", "5:00 PM"] }
-  ]
+    tiktok: ""
+  }
 };
